f90d0cedc0c9b4472c895c5c5dfc786f0627b8e8
hyperxpizza <wojtek.frackowski@gmail.com>
2021-12-23 20:50:06 +0100

    grpc functions signatures

---
+++ pkg/grpc/impl/server.go
@@ -0,0 +1,32 @@
+package impl
+
+import (
+	"context"
+
+	"github.com/hyperxpizza/cdn/pkg/config"
+	pb "github.com/hyperxpizza/cdn/pkg/grpc"
+)
+
+type CDNServiceImpl struct {
+	pb.UnimplementedCDNGrpcServiceServer
+	cfg *config.Config
+}
+
+func NewCDNService(c *config.Config) CDNServiceImpl {
+	return CDNServiceImpl{
+		cfg: c,
+	}
+}
+
+func (c *CDNServiceImpl) UploadFile(req *pb.UploadFileRequest, stream pb.CDNGrpcService_UploadFileServer) error {
+	return nil
+}
+
+func (c *CDNServiceImpl) DownloadFile(req *pb.DownloadFileRequest, stream pb.CDNGrpcService_DownloadFileServer) error {
+	return nil
+}
+
+func (c *CDNServiceImpl) SearchFile(ctx context.Context, req *pb.SearchResponse) (*pb.SearchResponse, error) {
+	var resp pb.SearchResponse
+	return &resp, nil
+}
